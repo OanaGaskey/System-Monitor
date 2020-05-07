fea33ce215dabec02c2c561eaf72f01bb17987c5
Oana <oana.gaskey@gmail.com>
2020-05-07 18:03:34 -0400

    implemented parser and format for Up Time

--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,14 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+  int h{0};
+  int m{0};
+  int s{0};
+ 
+  h = seconds/3600;
+  m = seconds%3600;
+  s = m%60;
+  m = m/60;
+  return std::to_string(h)+":"+std::to_string(m)+":"+std::to_string(s);
+}
--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -70,7 +70,19 @@ vector<int> LinuxParser::Pids() {
 float LinuxParser::MemoryUtilization() { return 0.0; }
 
 // TODO: Read and return the system uptime
-long LinuxParser::UpTime() { return 0; }
+long int LinuxParser::UpTime() { 
+  string line;
+  string value;
+  long int up_time {1};
+  std::ifstream filestream(kProcDirectory + kUptimeFilename);
+  if (filestream.is_open()) {
+    std::getline(filestream, line);
+    std::istringstream linestream(line);
+    linestream >> value;
+    up_time = std::stol(value);
+  }
+  return up_time;
+}
 
 // TODO: Read and return the number of jiffies for the system
 long LinuxParser::Jiffies() { return 0; }
