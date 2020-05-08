5379d83a93157bf83aec225803dfd4111623c98c
Oana <oana.gaskey@gmail.com>
2020-05-08 17:50:55 -0400

    parsed user id

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -202,11 +202,26 @@ string LinuxParser::Ram(int pid[[maybe_unused]]) { return string(); }
 
 // TODO: Read and return the user ID associated with a process
 // REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::Uid(int pid[[maybe_unused]]) { return string(); }
+string LinuxParser::Uid(int pid) { 
+  string line;
+  string key;
+  string value{};
+  std::ifstream filestream(kProcDirectory + "/" + std::to_string(pid) +"/status");
+  if(filestream.is_open()){
+    while(std::getline(filestream, line)){
+      std::istringstream linestream(line);
+      linestream >> key >> value;
+      if(key == "Uid:") {return value;}
+    }
+  }
+  return value; 
+}
 
 // TODO: Read and return the user associated with a process
 // REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::User(int pid[[maybe_unused]]) { return string(); }
+string LinuxParser::User(int pid[[maybe_unused]]) { 
+
+}
 
 // TODO: Read and return the uptime of a process
 // REMOVE: [[maybe_unused]] once you define the function
